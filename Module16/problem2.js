efd93c2f75069d6f77856220a3e8cf1f03e63f0c
BbillahrariBBr <mb.billah8@gmail.com>
2022-07-31 11:46:54 +0600

    module16 prob2

---
+++ Module16/problem2.js
@@ -0,0 +1,9 @@
+var mathematics = 75.25;
+var biology = 65;
+var chem = 80;
+var phy = 35.45;
+var bangla = 99.50;
+var total = mathematics + biology + chem + phy + bangla;
+console.log(total);
+var avg = (total / 5).toFixed(2);
+console.log(avg); 
