ecf712ef218f9e5bad35653977e1612292a163d1
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-04 11:57:20 +0600

    practice ch-4 5 done

---
+++ Module19/signal.js
@@ -0,0 +1,16 @@
+var signal = 'green';
+
+switch (signal) {
+    case 'red':
+        console.log("you may be in danger.");
+        break;
+    case 'yellow':
+        console.log("you should stop.");
+        break;
+    case 'green':
+        console.log("you should cross the road.");
+        break;
+    default:
+        console.log("Don't hurry up.");
+
+}
