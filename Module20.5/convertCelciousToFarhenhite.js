85ba580cd66452afcfda55a6dc0d5ded2da30537
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-05 23:41:36 +0600

    today extra practice 2 done

--- Module20.5/convertCelciousToFarhenhite.js
+++ Module20.5/convertCelciousToFarhenhite.js
@@ -4,5 +4,13 @@ function celTofar(cels) {
     return f;
 }
 
+function farToCel(far) {
+    // c = (f-32)/1.8;
+    let c = (far - 32) / 1.8;
+    return c;
+}
+
 let result = celTofar(30);
+let res = farToCel(86);
 console.log(result);
+console.log(res);
