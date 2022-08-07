6e3802b3b0b2422095b2d8373e53eee90c3b5722
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-07 21:15:16 +0600

    module 22 dne

---
+++ Module22/test.js
@@ -0,0 +1,11 @@
+const nayoks = ["Bangla Bhai", "English Bhai", "Korean Bhai ", "Turkish Bhai "];
+for (const nayok of nayoks) {
+    console.log(nayok);
+}
+console.log(Math.min(7, 1));
+
+let sum = 0;
+for (let i = 0; i <= 3; i++) {
+    sum = sum + i;
+}
+console.log(sum);
