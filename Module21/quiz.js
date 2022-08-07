f5069549a35b2a238a06bd3d45e1f4919b5ddde2
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-07 13:12:05 +0600

    mod 21 done

---
+++ Module21/quiz.js
@@ -0,0 +1,9 @@
+console.log(Math.round(Math.random() * 10));
+console.log(Math.floor(13.67));
+
+const number = -78;
+const answer = Math.abs(number);
+console.log(answer);
+
+var a = 2; var b = 3; if (a < b) { console.log("Hello"); }
+console.log(Math.floor(12.96));
