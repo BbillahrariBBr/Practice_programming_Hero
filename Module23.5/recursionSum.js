608bd0edc7629e56dc57238671306b9f9bd7de29
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-11 21:03:55 +0600

    module 23.5 done

--- Module23.5/recursionSum.js
+++ Module23.5/recursionSum.js
@@ -14,6 +14,29 @@ function recursion(number) {
 
     return number * recursion(number - 1);
 }
+function compare(a, b) {
+    if (a == b) {
+        return true;
+    } else {
+        return false;
+    }
 
-const res = recursion(6);
-console.log(res);
+}
+
+function comp(a, b) {
+    if (a.toString() === b) {
+        return true;
+    } else {
+        return false;
+    }
+}
+const result = comp(25, 25);
+console.log("123" + 123);
+let a = "hi";
+let b = "there";
+console.log(a + b);
+// console.log(result);
+// const result = compare(15, "15");
+// console.log(result);
+// const res = recursion(6);
+// console.log(res);
