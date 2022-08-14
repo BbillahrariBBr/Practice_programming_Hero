a1315f5cd2069871c57d14386d3903d93bd8a640
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-14 06:07:09 +0600

    event onclick change color using btn

--- Module25Event/js/event.js
+++ Module25Event/js/event.js
@@ -1,1 +1,12 @@
-console.log("Test event");
+//console.log("Test event");
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+const mBlue = document.getElementById('makeBlue');
+
+mBlue.onclick = blueButton;
+
+function blueButton() {
+    document.body.style.backgroundColor = 'blue';
+}
