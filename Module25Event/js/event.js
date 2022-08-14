15cc515095f4f213c0a67612ce49a7a38ff5e0f9
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-14 11:29:35 +0600

    add eventlistener

--- Module25Event/js/event.js
+++ Module25Event/js/event.js
@@ -10,3 +10,26 @@ mBlue.onclick = blueButton;
 function blueButton() {
     document.body.style.backgroundColor = 'blue';
 }
+const purpleBtn = document.getElementById("makePurple");
+purpleBtn.onclick = function makePurple() {
+    document.body.style.backgroundColor = 'purple';
+}
+
+const pinkButton = document.getElementById("make-pink");
+pinkButton.addEventListener('click', makePink)
+
+function makePink() {
+    document.body.style.backgroundColor = 'pink';
+}
+
+//
+const skyButton = document.getElementById('make-sky');
+
+skyButton.addEventListener('click', function makeSky() {
+    document.body.style.backgroundColor = 'SkyBlue';
+}
+)
+
+document.getElementById('make-green').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'green'
+})
