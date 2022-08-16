4ae1a6156b98bd2937b86bb4e72c90f7217dbf6b
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-16 17:38:30 +0600

    add event listener comapare email and pass

---
+++ Module26/js/login.js
@@ -0,0 +1,18 @@
+//step 1: add click event handler with submit button
+document.getElementById('btn-submit').addEventListener('click', function (event) {
+    //console.log(event.target);
+    //step 2: get the email address inside the email input field;
+    const emailValue = document.getElementById('inp-email').value;
+    const passValue = document.getElementById('inp-pass').value;
+    //console.log(emailValue, passValue);
+
+    // do not verify email password on the client side
+    // step-4 verify email and password
+    if (emailValue === "sontan@baap.com" && passValue === "secret") {
+        console.log("Valid user");
+    }
+    else {
+        console.log("Invalid user");
+    }
+
+});
