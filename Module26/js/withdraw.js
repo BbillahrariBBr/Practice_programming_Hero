42156187a6383e8e51eb2bd62db238760bc25bc5
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-17 11:15:02 +0600

    updat withdraw and total balance done

--- Module26/js/withdraw.js
+++ Module26/js/withdraw.js
@@ -2,19 +2,33 @@ document.getElementById('btn-withdraw').addEventListener('click', function (even
     const withdrawField = document.getElementById('inp-withdraw');
     const newWithdrawAmountString = withdrawField.value
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+    if (isNaN(newWithdrawAmount)) {
+        alert("plz provide a valid number");
+        withdrawField.value = '';
+        return;
+    }
 
     const withdrawTotalView = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalView.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+
+
+
+    const previousTotalBalanceId = document.getElementById('total-balance');
+    const previousTotalBalance = parseFloat(previousTotalBalanceId.innerText);
+    if (newWithdrawAmount > previousTotalBalance) {
+        alert("Baap er bank a eto taka nai");
+        withdrawField.value = '';
+        return;
+    }
 
     const currentWithdrawTotal = newWithdrawAmount + previousWithdrawTotal;
 
     withdrawTotalView.innerText = currentWithdrawTotal;
     withdrawField.value = '';
 
-
-    const previousTotalBalanceId = document.getElementById('total-balance');
-    const previousTotalBalance = parseFloat(previousTotalBalanceId.innerText);
     const currentTotalBalance = previousTotalBalance - newWithdrawAmount;
+
     previousTotalBalanceId.innerText = currentTotalBalance;
 })
