2e7d3833ccfbfe813ff768ae9a5683b915ddba89
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-17 12:44:14 +0600

    withdraw update

--- Module27/js/withdraw.js
+++ Module27/js/withdraw.js
@@ -1,1 +1,12 @@
-console.log("withdraw js file");
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+    const newWithdraw = getInputValueById('inp-withdraw')
+    //console.log(newWithdraw);
+    const previousWithdraw = getInnerTextValueById('withdraw-total')
+    //console.log(previousWithdraw);
+    const withdrawTotal = newWithdraw + previousWithdraw;
+    setInnerTextValueById('withdraw-total', withdrawTotal);
+
+    const previousTotalBalance = getInnerTextValueById('total-balance')
+    const newTotalBalance = previousTotalBalance - newWithdraw;
+    setInnerTextValueById('total-balance', newTotalBalance);
+})
