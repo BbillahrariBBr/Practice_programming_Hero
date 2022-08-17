edd31d986c472abc321b4e426f43cc2455a4e1f0
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-17 10:29:44 +0600

    updat deposit and total balance

--- Module26/js/deposit.js
+++ Module26/js/deposit.js
@@ -1,8 +1,25 @@
 document.getElementById("btn-deposit").addEventListener("click", function (event) {
     const depositField = document.getElementById('inp-deposit');
-    const depositAmount = depositField.value;
+    const newDepositAmountString = depositField.value
+    const newDepositAmount = parseFloat(newDepositAmountString);
+    // console.log(newDepositAmount);
+    // console.log(typeof newDepositAmount);
     const depositTotalView = document.getElementById('deposit-total');
-    const depositTotal = depositTotalView.value;
-    depositTotalView.innerText = depositAmount;
-    //console.log(depositAmount);
+    const previousDepositTotalString = depositTotalView.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+    // console.log(previousDepositTotal);
+    // console.log(typeof previousDepositTotal);
+
+
+    const currentDepositTotal = newDepositAmount + previousDepositTotal;
+    //console.log(typeof currentDepositTotal);
+    // console.log(currentDepositTotal);
+    depositTotalView.innerText = currentDepositTotal;
+    depositField.value = '';
+    const previousTotalBalanceId = document.getElementById('total-balance');
+    const previousTotalBalance = parseFloat(previousTotalBalanceId.innerText);
+    const currentTotalBalance = previousTotalBalance + newDepositAmount;
+    previousTotalBalanceId.innerText = currentTotalBalance
+    //cons
+    //console.log(newDepositAmount);
 })
