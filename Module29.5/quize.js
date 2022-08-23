99650c5fbba6e4ed03fb73200f26e8e601075f0d
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-23 22:49:49 +0600

    module 30 done with practice

--- Module29.5/quize.js
+++ Module29.5/quize.js
@@ -1,5 +1,30 @@
 function getSum(a, b = 9) { return a + b; } console.log(getSum(2, 7));
 const y = x => x * x; const z = y(5)
 //console.log(z);
-const division = num1, num2 => console.log(num1 / num2);
+const division = (num1, num2) => console.log(num1 / num2);
 division(4, 2)
+const three = (nmb1, nmb2, nmb3) => nmb1 * nmb2 * nmb3;
+console.log(three(1, 2, 3));
+const mulLine = `I am a web Developer.
+I Love to code.
+I Love to eat biryani`;
+console.log(mulLine);
+
+const friends = ['saki', 'bbillah', 'ela', 'bushra'];
+const evenFrnd = []
+// for (friend of friends) {
+//     if (friend.length % 2 == 0) {
+//         evenFrnd.push(friend);
+//     }
+// }
+//console.log(evenFrnd);
+const f1 = (array) => {
+    for (friend of array) {
+        if (friend.length % 2 == 0) {
+            evenFrnd.push(friend);
+        }
+    }
+    return evenFrnd
+}
+//const lis = f1(friends)
+console.log(f1(friends));
