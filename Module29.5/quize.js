e7f312bc73fd00ef6d3a56aafdb0cd9e6848da63
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-23 22:34:44 +0600

    module 30 done

---
+++ Module29.5/quize.js
@@ -0,0 +1,5 @@
+function getSum(a, b = 9) { return a + b; } console.log(getSum(2, 7));
+const y = x => x * x; const z = y(5)
+//console.log(z);
+const division = num1, num2 => console.log(num1 / num2);
+division(4, 2)
