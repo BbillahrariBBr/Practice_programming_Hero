43e8bc942a120eee8ca3ed7ce68194675d2b4e3a
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-23 21:58:57 +0600

    more arrow func

--- Module29.5/arrow.js
+++ Module29.5/arrow.js
@@ -19,3 +19,12 @@ const add12 = function (nmbr1, nmbr2) {
 //arrow function-> variable name = (parameters) => return value;
 const add5 = (nmbr1, nmbr2 = 0) => nmbr1 + nmbr2;
 console.log(add5(5));
+
+// no parameter arrow function
+const getPie = () => 3.1416;
+
+//single parameter
+const doubleIt = (nmbr) => nmbr * 2;
+
+//single param simplified ver
+const fiveTimes = nmbr => nmbr * 5;
