543ea6e17fcabd1a3b62669b26c8578ae1250863
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-23 21:23:54 +0600

    date.js

---
+++ Module29.5/date.js
@@ -0,0 +1,10 @@
+const today = new Date();
+console.log(today);
+const date1 = new Date('1971-03-26');
+const date2 = new Date('1971-12-16');
+if (date1 > date2) {
+    console.log("march was before dec");
+}
+else {
+    console.log("march wasn't before dec");
+}
