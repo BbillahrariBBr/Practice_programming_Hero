d424010e70863cb85250d7de94f5c628f1936464
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-23 19:28:08 +0600

    try catch finally

---
+++ Module29.5/try-catch.js
@@ -0,0 +1,19 @@
+const day = 'day the din';
+console.log(day);
+console.log(25);
+
+try {
+    console.log(district);
+} catch (error) {
+    //console.log(error);
+
+}
+finally {
+    console.log(
+        71
+    );
+}
+let district = 'Bbaria';
+console.log(
+    61
+);
