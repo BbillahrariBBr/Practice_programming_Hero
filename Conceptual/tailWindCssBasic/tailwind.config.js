0dc454a70f4fe04ab1b6de83d5991d0f587247e8
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-24 18:29:36 +0600

    almost success tailwind install using cli

--- Conceptual/tailWindCssBasic/tailwind.config.js
+++ Conceptual/tailWindCssBasic/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',],
   theme: {
     extend: {},
   },
