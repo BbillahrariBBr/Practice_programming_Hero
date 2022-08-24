41f28d89a40325d56145210c8ef153816c5de5ca
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-25 00:23:59 +0600

    class

---
+++ Module31Es6Advanced/extends.js
@@ -0,0 +1,44 @@
+class Instructor {
+    name;
+    designation = 'web Course Instructor';
+    team = 'Web Team';
+    location;
+    constructor(name, location) {
+        this.name = name;
+        this.location = location;
+
+    }
+    startSupportSession(time) {
+        console.log(`start the support session at ${time}`);
+    }
+    createQuize(module) {
+        console.log(`plz create quiz for module ${module}`);
+    }
+    provideFeedback() {
+        console.log(`${this.name} thank u for ur feedback`);
+    }
+}
+
+
+class Developer {
+    name;
+    designation = 'web Course Instructor';
+    team = 'Web Team';
+    location;
+    tech;
+    constructor(name, location, tech) {
+        this.name = name;
+        this.location = location;
+        this.tech = tech;
+
+    }
+    developFeature(feature) {
+        console.log(`plz develop the  ${feature}`);
+    }
+    release(version) {
+        console.log(`plz create quiz for module ${version}`);
+    }
+    provideFeedback() {
+        console.log(`${this.name} thank u for ur feedback`);
+    }
+}
