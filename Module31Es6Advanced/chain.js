ce7598e75a5902432e61c8cd2126d89179ed4147
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-24 22:22:15 +0600

    chain

---
+++ Module31Es6Advanced/chain.js
@@ -0,0 +1,13 @@
+const users = [
+    { id: 1, name: 'abul', job: 'doctor' }
+]
+//console.log(users[0].name);
+
+const data = {
+    count: 500,
+    data: [
+        { id: 1, name: 'abul', job: 'doctor' },
+        { id: 2, name: 'dabul', job: 'leader' },
+    ]
+}
+console.log(data.data[0].job);
