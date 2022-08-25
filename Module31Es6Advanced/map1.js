c700fb49e8291ff8dafc4be5c86d1290d64b11f1
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-25 10:45:48 +0600

    map edit

--- Module31Es6Advanced/map1.js
+++ Module31Es6Advanced/map1.js
@@ -1,9 +1,14 @@
 const oddArray = [1, 3, 5, 7, 9];
+const eveArray = [];
+for (arr of oddArray) {
+    eveArray.push(arr + 1);
+}
 const evenArray = oddArray.map(x => x + 1);
 // console.log(evenArray);
+console.log(eveArray);
 
 const anArray = [33, 50, 79, 78, 90, 101, 30];
 const divTen = anArray.filter(x => x % 10 == 0);
-console.log(divTen);
+// console.log(divTen);
 const findFirst1 = anArray.find(x => x % 10 == 0);
-console.log(findFirst1);
+// console.log(findFirst1);
