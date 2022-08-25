2db2f9b594cb14958c564697373e3b152d67f137
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-25 10:35:51 +0600

    practice 2 filter done

--- Module31Es6Advanced/map1.js
+++ Module31Es6Advanced/map1.js
@@ -1,3 +1,7 @@
 const oddArray = [1, 3, 5, 7, 9];
 const evenArray = oddArray.map(x => x + 1);
-console.log(evenArray);
+// console.log(evenArray);
+
+const anArray = [33, 50, 79, 78, 90, 101, 30];
+const divTen = anArray.filter(x => x % 10 == 0);
+console.log(divTen);
