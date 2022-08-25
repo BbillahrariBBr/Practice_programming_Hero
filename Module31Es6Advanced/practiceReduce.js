72afde2d54b7d1966d394099dc2f35a31e0e7bb6
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-25 11:13:16 +0600

    challenge reduce done

--- Module31Es6Advanced/practiceReduce.js
+++ Module31Es6Advanced/practiceReduce.js
@@ -6,4 +6,12 @@ for (arr of anArray) {
 
 // console.log(sum);
 const s = anArray.reduce((p, r) => p + r, 0);
-console.log(s);
+// console.log(s);
+
+const people = [
+    { name: 'meena', age: 20 },
+    { name: 'Rina', age: 15 },
+    { name: 'Suchorita', age: 22 }
+]
+const output = people.reduce((p, c) => p + c.age, 0)
+console.log(output);
