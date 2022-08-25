1b89eec876c320a2eaeae9329a772d4c89e5a34b
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-25 11:20:49 +0600

    access obj chalng 2 done

--- Module31Es6Advanced/accessObjectVal.js
+++ Module31Es6Advanced/accessObjectVal.js
@@ -2,4 +2,15 @@ const student = {
     name: 'Fredie',
     age: 26,
 }
-console.log(student.age);
+// console.log(student.age);
+const data = {
+    location: [
+        {
+            lat: '34.5 , 37.8',
+            long: '98.77 , 58.7',
+            city: 'Hyderabad',
+            country: 'India'
+        }
+    ]
+}
+console.log(data.location[0].city);
