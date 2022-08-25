ed0cdab394eb173cdf8fd059c96c5629a62b6636
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-25 10:38:23 +0600

    practice 3 find done

--- Module31Es6Advanced/map1.js
+++ Module31Es6Advanced/map1.js
@@ -5,3 +5,5 @@ const evenArray = oddArray.map(x => x + 1);
 const anArray = [33, 50, 79, 78, 90, 101, 30];
 const divTen = anArray.filter(x => x % 10 == 0);
 console.log(divTen);
+const findFirst1 = anArray.find(x => x % 10 == 0);
+console.log(findFirst1);
