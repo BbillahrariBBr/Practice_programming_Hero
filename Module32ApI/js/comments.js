e7b380aaa895d064f0b4ff52dfc89738c50e7471
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-27 07:23:58 +0600

    fetch practice using json placeholder comments done

---
+++ Module32ApI/js/comments.js
@@ -0,0 +1,24 @@
+function comments() {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+        .then(res => res.json())
+        .then(data => displayData(data))
+}
+
+function displayData(userComments) {
+    const userCommentContainer = document.getElementById('comments-container');
+    for (userComment of userComments) {
+        const commentDiv = document.createElement('div');
+        commentDiv.innerHTML = `
+        <div>
+        <p>id: ${userComment.id}</p>
+        <p>name: ${userComment.name} </p>
+        <p>mail: ${userComment.email} </p>
+        <p>comment: ${userComment.body}</p>
+        </div>
+        `
+        commentDiv.classList.add('post')
+        userCommentContainer.appendChild(commentDiv)
+        console.log(userComment);
+    }
+}
+comments();
