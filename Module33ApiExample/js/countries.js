43f928d986d54be2aa0666bdf5158ead1c646eba
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-30 12:05:02 +0600

    dynamic url

--- Module33ApiExample/js/countries.js
+++ Module33ApiExample/js/countries.js
@@ -17,6 +17,7 @@ const displayCountries = countries => {
         countryDiv.innerHTML = `
         <h3>Country Name: ${country.name.common}</h3>
         <p>Capital: ${country.capital ? country.capital[0] : 'No capital'}</p>
+        <button onclick="loadCountryDetail('${country.cca2}')">Details</button>
         `;
         countriesContainer.appendChild(countryDiv);
         //console.log(country);
@@ -24,4 +25,26 @@ const displayCountries = countries => {
     //using map
     //console.log(countries);
 }
+const loadCountryDetail = (code) => {
+    // https://restcountries.com/v3.1/alpha/{code}
+    const url = `https://restcountries.com/v3.1/alpha/${code}
+    `;
+    //data load
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDetail(data[0]))
+    // console.log(url);
+    // console.log("get Country Detail", code);
+    // cons
+}
+
+const displayCountryDetail = (country) => {
+    const countryDetail = document.getElementById('country-detail');
+    countryDetail.innerHTML = `
+                <h2>Official Name: ${country.name.official} </h2>
+                <img src="${country.flags.png}" alt="">
+    `
+    console.log(country);
+}
+
 loadCountries();
