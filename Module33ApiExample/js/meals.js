55a46b8dee4dd79083d2b9ac25967c43a2701b06
BbillahrariBBr <mb.billah8@gmail.com>
2022-09-01 11:21:15 +0600

    dynamic search

--- Module33ApiExample/js/meals.js
+++ Module33ApiExample/js/meals.js
@@ -1,11 +1,13 @@
-const loadMeals = () => {
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+const loadMeals = (search) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
+    fetch(url)
         .then(res => res.json())
         .then(data => displayMeal(data.meals))
 }
 
 const displayMeal = (meals) => {
     const mealContainer = document.getElementById('meal-container');
+    mealContainer.innerHTML = ``;
     meals.forEach(meal => {
         const mealDiv = document.createElement('div');
         mealDiv.classList.add('col');
@@ -14,7 +16,7 @@ const displayMeal = (meals) => {
                         <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">${meal.strMeal}</h5>
-                            <p class="card-text">${meal.strInstructions}</p>
+                            <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
                         </div>
                     </div>
         `;
@@ -22,4 +24,9 @@ const displayMeal = (meals) => {
         console.log(meal);
     });
 }
-loadMeals()
+
+document.getElementById('search-btn').addEventListener('click', function () {
+    const searchText = document.getElementById('search-text').value;
+    loadMeals(searchText);
+    document.getElementById('search-text').value = '';
+})
