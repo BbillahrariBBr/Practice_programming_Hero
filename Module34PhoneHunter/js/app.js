be596afeff20656b4f27b42a3cc950d733381de5
BbillahrariBBr <mb.billah8@gmail.com>
2022-09-01 16:44:49 +0600

    modal 34-7 5.28

--- Module34PhoneHunter/js/app.js
+++ Module34PhoneHunter/js/app.js
@@ -38,7 +38,7 @@ const displayPhones = (data, dataLimit) => {
                     <h5 class="card-title">${phone.phone_name}</h5>
                     <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
 
-                    <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
+                    <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
             </div>
         `;
         phoneContainer.appendChild(phonDiv);
