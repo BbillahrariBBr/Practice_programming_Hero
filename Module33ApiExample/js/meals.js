604dbdf4ab3b3647b02fdf4a05a7c4ec0d47835d
BbillahrariBBr <mb.billah8@gmail.com>
2022-09-01 12:33:38 +0600

    dynamic url done

--- Module33ApiExample/js/meals.js
+++ Module33ApiExample/js/meals.js
@@ -12,7 +12,7 @@ const displayMeal = (meals) => {
         const mealDiv = document.createElement('div');
         mealDiv.classList.add('col');
         mealDiv.innerHTML = `
-                    <div class="card h-100">
+                    <div onclick="loadMealDetails(${meal.idMeal})" class="card h-100">
                         <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">${meal.strMeal}</h5>
@@ -21,7 +21,7 @@ const displayMeal = (meals) => {
                     </div>
         `;
         mealContainer.appendChild(mealDiv);
-        console.log(meal);
+        // console.log(meal);
     });
 }
 
@@ -30,3 +30,32 @@ document.getElementById('search-btn').addEventListener('click', function () {
     loadMeals(searchText);
     document.getElementById('search-text').value = '';
 })
+
+const loadMealDetails = (idMeal) => {
+    // console.log(idMeal);
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayLoadMealDetails(data.meals[0]))
+
+}
+
+const displayLoadMealDetails = (mealData) => {
+    const mealContainer = document.getElementById('meal-card');
+    mealContainer.innerHTML = ``;
+    mealCard = document.createElement('div');
+    mealCard.innerHTML = `
+        <div class="card" style="width: 18rem;">
+            <img src="${mealData.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${mealData.strMeal}</h5>
+                <p class="card-text">${mealData.strInstructions.slice(0, 200)}</p>
+            </div>
+        </div>
+    `;
+    mealContainer.appendChild(mealCard);
+
+    console.log(mealData);
+}
+loadMeals('');
