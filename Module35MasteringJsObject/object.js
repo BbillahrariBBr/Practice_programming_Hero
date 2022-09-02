aef6730be8fbd20b1f6db54a5afe969c2e13dbc1
BbillahrariBBr <mb.billah8@gmail.com>
2022-09-02 20:09:14 +0600

    Module introduction Different way to Create Object

---
+++ Module35MasteringJsObject/object.js
@@ -0,0 +1,27 @@
+// create object using object literal
+const player = {};
+player.name = 'Small Nirob';
+player.specialty = 'batsman';
+player.bat = function () {
+    console.log('Swing ur bat');
+}
+// console.log(player);
+
+//object constructor
+
+const person = new Object;
+person.name = 'Solaman';
+// console.log(person);
+
+//object create method
+
+const item = Object.create(null);
+item.name = 'Phone';
+console.log(item);
+
+//class
+// oop er moto
+
+
+//function
+//old version
