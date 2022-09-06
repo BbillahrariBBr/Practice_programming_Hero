6834ffd3a8a32f7b18fbdfd34d480cbacf777723
BbillahrariBBr <mb.billah8@gmail.com>
2022-09-06 12:25:35 +0600

    module 38 done

--- Module38BrowserApi/js/cart.js
+++ Module38BrowserApi/js/cart.js
@@ -9,16 +9,46 @@ const addProduct = () => {
     const name = getInputValueByID('prodcut-name-field');
     const quantity = getInputValueByID('product-quantity-field');
     console.log(name, quantity);
-    displayProducts(name, quantity);
+    addProductToDisplay(name, quantity);
 
     // set to local storage
     // simple way;
-    localStorage.setItem(name, quantity);
+    // localStorage.setItem(name, quantity);
+    saveItemToLocalStorage(name, quantity);
 }
 
-const displayProducts = (product, quantity) => {
+const getShoppingCartFromLocalStorage = () => {
+    let cart = {};
+    let savedCart = localStorage.getItem('cart');
+    if (savedCart) {
+        cart = JSON.parse(savedCart);
+    }
+    return cart;
+}
+
+const saveItemToLocalStorage = (product, quantity) => {
+    const cart = getShoppingCartFromLocalStorage();
+    //add product to the  object property;
+    cart[product] = quantity;
+    const cartStringify = JSON.stringify(cart);
+
+    //save to local storage
+    localStorage.setItem('cart', cartStringify);
+}
+
+const addProductToDisplay = (product, quantity) => {
     const productContainer = document.getElementById('product-container');
     const li = document.createElement('li');
     li.innerText = `${product} : ${quantity}`;
     productContainer.appendChild(li);
 }
+
+const displayStoredProducts = () => {
+    const cart = getShoppingCartFromLocalStorage();
+    for (const product in cart) {
+        const quantity = cart[product];
+        console.log(product, quantity);
+        addProductToDisplay(product, quantity);
+    }
+}
+displayStoredProducts()
