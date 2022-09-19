13012b4a3493ff07481e6628773cade9ee03045f
BbillahrariBBr <mb.billah8@gmail.com>
2022-09-19 23:55:00 +0600

    State

--- Module45SimpleReact/reactjs-core-concepts2/src/App.js
+++ Module45SimpleReact/reactjs-core-concepts2/src/App.js
@@ -1,22 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const products = [
-    { name: "Laptop", price: 60000 },
-    { name: "phone", price: 90000 },
-    { name: "watch", price: 600 },
-  ]
+
   return (
 
     <div className="App">
-      {
-        products.map(product => <Product name={product.name} price={product.price}></Product>)
-      }
+      <Counter></Counter>
 
     </div>
   );
 }
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const increase = () => setCount(count + 1);
+  const decrease = () => setCount(count - 1);
+  return (
+    <div>
+      <h1>COunt: {count} </h1>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+
+    </div>
+  )
+}
+/**
+ * const products = [
+    { name: "Laptop", price: 60000 },
+    { name: "phone", price: 90000 },
+    { name: "watch", price: 600 },
+  ]
+
+  {
+        products.map(product => <Product name={product.name} price={product.price}></Product>)
+      }
+
+ */
 function Product(props) {
   return (
     <div className='product'>
