6037be6a9a5701ae447508631e44ef02583b2b38
BbillahrariBBr <mb.billah8@gmail.com>
2022-09-19 22:06:08 +0600

    JSX, Dynamic content

--- Module45SimpleReact/reactjs-core-concepts/src/App.js
+++ Module45SimpleReact/reactjs-core-concepts/src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 5555;
+const singer = { name: 'Dr. Mahfuz', job: 'singer' };
+const singerStyle = {
+  color: 'purple',
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div className="container">
+          <h3>hello dear how are you</h3>
+        </div>
+        <div className="music">
+          <p>name:{number} </p>
+          <p style={singerStyle}>{singer.name}</p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
