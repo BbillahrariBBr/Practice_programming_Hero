3d179fb67475782c7e8dcd03aeca39c3d0c057ed
BbillahrariBBr <mb.billah8@gmail.com>
2022-09-19 23:05:38 +0600

    Create multiple components from an array of objects

--- Module45SimpleReact/reactjs-core-concepts/src/App.js
+++ Module45SimpleReact/reactjs-core-concepts/src/App.js
@@ -2,17 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 const number = 5555;
-const singer = { name: 'Dr. Mahfuz', job: 'singer' };
+const singers = [
+  { name: 'Dr. Mahfuz', job: 'singer' },
+  { name: 'Eva Rahman', job: 'singer2' },
+  { name: 'Agun', job: 'Sopno' },
+  { name: 'Shuvrodev', job: 'Eomn' }
+];
 const singerStyle = {
   color: 'purple',
 }
 function App() {
+  const nayoks = ['Rubel', 'BappaRaz', 'Kuber', 'Montu'];
+  // nayoks.map(nayok=>)
   return (
     <div className="App">
-      <Person name="Rubel" nayika="Moushumi"></Person>
+      {
+        nayoks.map(nayok => <li>{nayok}</li>
+        )
+      }
+      {
+        nayoks.map(nayok => <Person name={nayok}></Person>)
+      }
+      {
+        singers.map(singer => <Person name={singer.name} nayika={singer.job}></Person>)
+      }
+
+      {/* <Person name={nayoks[0]} nayika="Moushumi"></Person>
       <Person name='BappaRaz' nayika="cheka"></Person>
       <Person name="kuber" nayika="Kopila"></Person>
-      <Person name="Montu" nayika="Tuni"></Person>
+      <Person name="Montu" nayika="Tuni"></Person> */}
     </div>
   );
 }
