c191d2fc033f35c4a425ace5bb6228b3455865cd
BbillahrariBBr <mb.billah8@gmail.com>
2022-09-23 23:15:47 +0600

    fake api

--- Module47/how-react-works/src/Components/Cosmetics/Cosmetics.js
+++ Module47/how-react-works/src/Components/Cosmetics/Cosmetics.js
@@ -1,17 +1,57 @@
 import React from 'react';
-import Shoes from '../../Shoes/Shoes';
-import { add } from '../../utilities/calculate';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Cosmetic from '../../Cosmetic/Cosmetic';
+// import Shoes from '../../Shoes/Shoes';
+// import { add } from '../../utilities/calculate';
 // import add from '../../utilities/calculate';
 
 const Cosmetics = () => {
-    const first = 55;
-    const second = 66;
-    const total = add(first, second)
+    const [cosmetics, setCosmetics] = useState([]);
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setCosmetics(data))
+    }, [])
+    // const cosmetics = [
+    //     {
+    //         "_id": "632de7623a87eacd6c8f6baf",
+    //         "price": 70,
+    //         "name": "Booth Black"
+    //     },
+    //     {
+    //         "_id": "632de76210d15554be246d72",
+    //         "price": 28,
+    //         "name": "Humphrey Swanson"
+    //     },
+    //     {
+    //         "_id": "632de76278da87add8af1140",
+    //         "price": 89,
+    //         "name": "Debbie Sykes"
+    //     },
+    //     {
+    //         "_id": "632de762aaac7d3f86cad993",
+    //         "price": 149,
+    //         "name": "Luna Dalton"
+    //     },
+    //     {
+    //         "_id": "632de76288a9572f5ad8b5e5",
+    //         "price": 290,
+    //         "name": "Cheri Gates"
+    //     }
+    // ]
+    // const first = 55;
+    // const second = 66;
+    // const total = add(first, second)
     return (
         <div>
             <h1>Welcome to my Cosmetics store</h1>
-            <p>total: {total}</p>
-            <Shoes></Shoes>
+            {
+                cosmetics.map(cosmetic => <Cosmetic
+                    cosmetic={cosmetic} key={cosmetic.id}></Cosmetic>)
+            }
+            {/* <p>total: {total}</p>
+            <Shoes></Shoes> */}
         </div>
     );
 };
