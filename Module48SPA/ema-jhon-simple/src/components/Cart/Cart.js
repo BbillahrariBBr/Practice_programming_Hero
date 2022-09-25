ee6d4910850d5607a4b7d58416804a5f927370e0
BbillahrariBBr <mb.billah8@gmail.com>
2022-09-25 22:35:56 +0600

    Setting the cart data on the localStorage for future retrieve

--- Module48SPA/ema-jhon-simple/src/components/Cart/Cart.js
+++ Module48SPA/ema-jhon-simple/src/components/Cart/Cart.js
@@ -2,10 +2,24 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({ cart }) => {
+    console.log(cart);
+    let total = 0;
+    let totalShipping = 0;
+    for (const product of cart) {
+        total += product.price;
+        totalShipping += product.shipping;
+    }
+    const tax = total * 0.1;
+    const grandTotal = total + totalShipping + tax;
     return (
-        <div >
+        <div className='cart' >
             <h3>order summary</h3>
             <p>Selected Items: {cart.length}</p>
+            <p>Total price: ${total} </p>
+            <p>Total Shipping:${totalShipping} </p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h5>Grand Total:${grandTotal.toFixed(2)} </h5>
+
         </div>
     );
 };
