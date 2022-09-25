55337be75fe42edb5979ab1852378ce4a3164cdd
BbillahrariBBr <mb.billah8@gmail.com>
2022-09-25 13:53:41 +0600

    Install external packages, react-fontawesome, display icon

--- Module48SPA/ema-jhon-simple/src/components/Product/Product.js
+++ Module48SPA/ema-jhon-simple/src/components/Product/Product.js
@@ -1,3 +1,5 @@
+import { faCartShopping, } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Product.css'
 
@@ -16,7 +18,8 @@ const Product = (props) => {
                 <p> <small>Ratings: {ratings} stars</small></p>
             </div>
             <button onClick={() => { handleClick(product) }} className='btn-cart'>
-                <p>Add to Cart</p>
+                <p className='btn-text'>Add to Cart</p>
+                <FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon>
             </button>
         </div>
     );
