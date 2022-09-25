4e0d9bf15d37813c41cbf1f21be07a50720e2c24
BbillahrariBBr <mb.billah8@gmail.com>
2022-09-25 13:22:26 +0600

    Cart state setup, update cart and total on click

--- Module48SPA/ema-jhon-simple/src/components/Product/Product.js
+++ Module48SPA/ema-jhon-simple/src/components/Product/Product.js
@@ -2,17 +2,20 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const { name, price, seller, ratings, img } = props.product;
+    const { handleClick, product } = props;
+    const { name, price, seller, ratings, img } = product;
+
+
     return (
         <div className='product'>
             <img src={img} alt="" />
             <div className='product-info'>
                 <p className='product-name'>{name}</p>
-                <p>Price: {price}</p>
-                <p><small>Seller: ${seller}</small></p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
                 <p> <small>Ratings: {ratings} stars</small></p>
             </div>
-            <button className='btn-cart'>
+            <button onClick={() => { handleClick(product) }} className='btn-cart'>
                 <p>Add to Cart</p>
             </button>
         </div>
