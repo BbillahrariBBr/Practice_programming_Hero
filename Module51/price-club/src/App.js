a3a36645c9d15467636c6f098001e0ca3fe071f0
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-01 20:28:40 +0600

    last part agin korte hobe

--- Module51/price-club/src/App.js
+++ Module51/price-club/src/App.js
@@ -1,7 +1,9 @@
 
 import './App.css';
+import AssignmentMarks from './AssignmentMarks/AssignmentMarks';
 import Navbar from './component/Navbar/Navbar';
 import Pricing from './component/Pricing/Pricing';
+import PhoneBar from './PhoneBar/PhoneBar';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Navbar></Navbar>
       <h3 className='text-4xl'>Hello</h3>
       <Pricing></Pricing>
+      <AssignmentMarks></AssignmentMarks>
+      <PhoneBar></PhoneBar>
 
     </div>
   );
