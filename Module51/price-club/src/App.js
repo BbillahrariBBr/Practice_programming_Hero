2d215bfc1ba06d9b3ba0572264756ecb61241174
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-01 18:21:33 +0600

    Responsive Navbar using react and tailwind

--- Module51/price-club/src/App.js
+++ Module51/price-club/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './component/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <h3 className='text-4xl'>Hello</h3>
+
     </div>
   );
 }
