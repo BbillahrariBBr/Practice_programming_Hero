0bd601e1f6b826983379846d50d7939317fa1244
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-01 19:35:17 +0600

    Simple Pricing Option using React and Tailwind

--- Module51/price-club/src/App.js
+++ Module51/price-club/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Navbar from './component/Navbar/Navbar';
+import Pricing from './component/Pricing/Pricing';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <h3 className='text-4xl'>Hello</h3>
+      <Pricing></Pricing>
 
     </div>
   );
