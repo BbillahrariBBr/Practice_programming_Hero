04def1294d65e64baa9ec3033774ea5665cd0005
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-08 14:42:24 +0600

    Load Users, display users, dynamic link, Not Found

--- Module52ReactRotes/explore-routes/src/components/Friends/Friends.js
+++ Module52ReactRotes/explore-routes/src/components/Friends/Friends.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
+    const frineds = useLoaderData()
     return (
         <div>
-            <h3>This is Friend  Page</h3>
+            <h3>This is Friend  Page {frineds.length}</h3>
+            {
+                frineds.map(friend => <Friend
+                    key={friend.id}
+                    friend={friend}
+                ></Friend>)
+            }
         </div>
     );
 };
