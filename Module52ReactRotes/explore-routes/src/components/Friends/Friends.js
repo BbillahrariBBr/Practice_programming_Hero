49bd3c343acd54ab961d7e505078061a54da40df
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-08 14:10:13 +0600

    Nested route and outlet for shared content

---
+++ Module52ReactRotes/explore-routes/src/components/Friends/Friends.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            <h3>This is Friend  Page</h3>
+        </div>
+    );
+};
+
+export default Friends;
--- Module52ReactRotes/explore-routes/src/components/Home/Home.js
+++ Module52ReactRotes/explore-routes/src/components/Home/Home.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Header from '../Header/Header';
 
 const Home = () => {
     return (
         <div>
-            <Header></Header>
+            {/* <Header></Header> */}
             <h3>This is the real Home Component</h3>
         </div>
     );
