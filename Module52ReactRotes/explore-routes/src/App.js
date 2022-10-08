793d5e3a847c646b232cd5bc888927847ab2183b
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-08 11:05:25 +0600

    learn simple react route

---
+++ Module52ReactRotes/explore-routes/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+function App() {
+  const router = createBrowserRouter([
+    { path: '/', element: <div>This is default page</div> },
+    { path: 'home', element: <div> Home Page</div> },
+    { path: 'about', element: <div>About page</div> },
+  ])
+  return (
+    <div className="App">
+
+      <RouterProvider router={router}></RouterProvider>
+
+    </div>
+  );
+}
+
+export default App;
