536f6ebea29ba89573717b117d83a7909906729f
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-11 15:27:49 +0600

    54 Prop Drilling to pass data to a many level components

--- Module54ReactRoutes/t-shirt-mania/src/component/Grandpa/Grandpa.js
+++ Module54ReactRoutes/t-shirt-mania/src/component/Grandpa/Grandpa.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import './Grandpa.css'
 import Father from '../Father/Father'
 import Uncle from '../Uncle/Uncle'
 import Aunty from '../Aunty/Aunty'
 
+const RingContext = createContext('Matir ring')
+
 const Grandpa = () => {
 
     const house = 7
+    const ring = 'Diamond ring'
     return (
         <div className='grandpa'>
             <h2>Grand pa</h2>
             <section className='flex'>
-                <Father house={house}></Father>
+                <Father house={house} ring={ring}></Father>
                 <Uncle house={house}></Uncle>
                 <Aunty house={house}></Aunty>
             </section>
--- Module54ReactRoutes/t-shirt-mania/src/component/Myself/Myself.js
+++ Module54ReactRoutes/t-shirt-mania/src/component/Myself/Myself.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Special from '../Special/Special';
 
-const Myself = ({ house }) => {
+const Myself = ({ house, ring }) => {
     return (
         <div>
             <h3>Myself</h3>
             <p><small>{house}</small></p>
+            <section>
+                <Special ring={ring}></Special>
+            </section>
         </div>
     );
 };
