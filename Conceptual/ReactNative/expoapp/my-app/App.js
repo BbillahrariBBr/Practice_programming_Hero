c9fb5c3a59494ae715197b1f2e4fd3f8d80fb161
BbillahrariBBr <mb.billah8@gmail.com>
2023-02-03 11:41:34 +0600

    0-5 Handling States and Events input text and button

--- Conceptual/ReactNative/expoapp/my-app/App.js
+++ Conceptual/ReactNative/expoapp/my-app/App.js
@@ -1,15 +1,27 @@
-import { View, Text, StyleSheet, Image } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Image, TextInput, Button } from 'react-native'
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import Home from './components/Home'
 
 export default function App() {
+  const [name, SetName] = useState("");
+  const [confirm, SetConfirm] = useState(false)
+  const handleName = (e) => {
+    SetName(e.nativeEvent.text)
+  };
   return (
     <View style={styles.app}>
       <Home />
       <Image style={styles.logo} source={require("./images/logo.jpg")} />
       <Image style={styles.logo}
         source={{ uri: "https://logos.flamingtext.com/Name-Logos/Treva-design-china-name.png" }} />
+      <TextInput placeholder='Write Your Name' style={styles.inputText} onChange={handleName} />
+      <Button title='Confirm' onPress={() => SetConfirm(true)} />
+      <Button title='Clear' onPress={() => {
+        SetConfirm(false);
+        SetName = "";
+      }} />
+      {confirm && name}
       <StatusBar style='auto' />
     </View>
   )
@@ -25,4 +37,8 @@ const styles = StyleSheet.create({
     height: 50,
     width: 100,
   },
+  inputText: {
+    borderWidth: 1,
+    borderColor: "red",
+  }
 })
