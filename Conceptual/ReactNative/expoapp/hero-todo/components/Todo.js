497c9358e284c7b5e92e9b58721041b399516010
BbillahrariBBr <mb.billah8@gmail.com>
2023-02-03 14:44:36 +0600

    0-8 Build and design main app and components part two

--- Conceptual/ReactNative/expoapp/hero-todo/components/Todo.js
+++ Conceptual/ReactNative/expoapp/hero-todo/components/Todo.js
@@ -6,26 +6,54 @@ export default function Todo() {
         <View style={styles.todo}>
             <View style={styles.todoNumber}>
                 <View style={styles.order}>
-                    <Text>#1</Text>
+                    <Text style={styles.textColor}>#1</Text>
                 </View>
                 <Text>Task One</Text>
             </View>
-            <TouchableOpacity style={styles.btn}></TouchableOpacity>
+            <TouchableOpacity style={styles.btn}>
+                <Text style={styles.textColor}>Remove</Text>
+            </TouchableOpacity>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     todo: {
+        backgroundColor: "#f7f7f7",
+        padding: 20,
+        borderRadius: 10,
+        flexDirection: "row",
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 10,
 
     },
     todoNumber: {
+        flexDirection: "row",
+        alignItems: 'center',
+        flexWrap: 'wrap',
 
     },
     order: {
+        width: 25,
+        height: 25,
+        backgroundColor: '#9b59b6',
+        opacity: .8,
+        borderRadius: 5,
+        marginRight: 10,
+        flexDirection: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
 
     },
     btn: {
+        backgroundColor: "#9b59b6",
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        borderRadius: 10,
 
+    },
+    textColor: {
+        color: '#fff',
     }
 })
