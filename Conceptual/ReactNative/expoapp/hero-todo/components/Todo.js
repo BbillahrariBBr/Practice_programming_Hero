d621db84ee7c6a88a01292e4e49a116b600ffad6
BbillahrariBBr <mb.billah8@gmail.com>
2023-02-03 12:36:36 +0600

    0-7 Build and design main app and components part one

---
+++ Conceptual/ReactNative/expoapp/hero-todo/components/Todo.js
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function Todo() {
+    return (
+        <View style={styles.todo}>
+            <View style={styles.todoNumber}>
+                <View style={styles.order}>
+
+                </View>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    todo: {
+
+    },
+    todoNumber: {
+
+    },
+    order: {
+
+    },
+})
